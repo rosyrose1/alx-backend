9143c892c0d4dbcbe2dd7ae14a74530a96876128
rosyrose1 <rosinaantwi96@gmail.com>
2024-03-12 11:57:25 -0700

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
-
-
-from flask import Flask, render_template, request
+""" Parametrize templates"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-class Config(object):
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """ configuration setup"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
@@ -26,21 +18,16 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale() -> str:
+    """ getter for web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def index():
-    """_summary_
-    """
+def get_index() -> str:
+    """ route handler"""
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port=5000)
